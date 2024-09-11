ea408a479f74b9dd72d5d61ea4c060668c2a1b97
Koichiro <tokails188s@gmail.com>
2024-09-11 17:02:45 +0900

    first commit

---
+++ abc081_a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+    int count=0;
+
+    for(char b:a){
+        if(b == '1') count+=1;
+    }
+    cout << count << endl;
+    return 0;
+}
