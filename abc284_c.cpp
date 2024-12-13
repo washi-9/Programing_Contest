50607c502a36be1f7f15a5f1ba6eb292f04b173f
Koichiro <tokails188s@gmail.com>
2024-12-14 05:48:55 +0900

    AC 入力のメモリ不足

--- abc284_c.cpp
+++ abc284_c.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N, M, A[109], B[109];
+int N, M, A[10009], B[10009];
 vector<int> G[100009]; // 隣接リスト
-bool visited[109*109];
+bool visited[100009];
 int result = 0;
 
 void dfs(int pos) {
@@ -20,19 +20,13 @@ void dfs(int pos) {
 
 int main() {
     cin >> N >> M;
-    if (M == 0) {
-        cout << N << endl;
-        return 0;
-    }
     for (int i = 1; i <= N; ++i) {
-        visited[i] = true;
+        visited[i] = false;
     }
     for (int i = 1; i <= M; ++i) {
         cin >> A[i] >> B[i];
         G[A[i]].push_back(B[i]);
         G[B[i]].push_back(A[i]);
-        visited[A[i]] = false;
-        visited[B[i]] = false;
     }
     for (int i = 1; i <= N; ++i) {
         if (visited[i] == false) {
